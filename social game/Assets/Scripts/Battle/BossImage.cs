662a6d4579448a17d456614f6e40ef6993754d05
yamakan-z <h1202016006@hiratagakuen.onmicrosoft.com>
2023-02-02 16:35:30 +0900

    後ボス3体

--- social game/Assets/Scripts/Battle/BossImage.cs
+++ social game/Assets/Scripts/Battle/BossImage.cs
@@ -17,17 +17,7 @@ public class BossImage : MonoBehaviour
 
     BattleManager b_manager;//バトルマネージャースクリプト
 
-    [SerializeField] private GameObject Stage1Button;//もうめんどいのでここでボタンの処理もする
-
-    [SerializeField] private GameObject[] CreateButton;//生成するボタン
-
-    [SerializeField] private GameObject[] ButtonGene;
-
-    [SerializeField] private float changetime;//ボタン変更時間
-
     public int max_BossHP;//ボスの最大HP
-
-    private bool onechange;//一回の処理につき一回ボタンチェンジ
 
     // Start is called before the first frame update
     void Start()
@@ -41,13 +31,10 @@ public class BossImage : MonoBehaviour
         if (b_manager.Stage_Id==1)
         {
             m_Image.sprite = m_Sprite[0];
-            Stage1Button.SetActive(true);
         }
         else if (b_manager.Stage_Id == 2)
         {
             m_Image.sprite = m_Sprite[1];
-            Stage1Button.SetActive(true);
-            //Instantiate(CreateButton[0], Legs[randnum].transform.position, Quaternion.identity);
         }
         else if (b_manager.Stage_Id == 3)
         {
@@ -67,30 +54,8 @@ public class BossImage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //ボスのHPが半分切った時,ボタンチェンジ
-        if(b_manager.Stage_Id == 2 && b_manager.B_hp <= max_BossHP / 2)
-        {
-            Debug.Log("ひか");
-            changetime -= Time.deltaTime;
-
-            if(changetime<=0 && !onechange)
-            {
-                Stage1Button.SetActive(false);
-                onechange = true;
-                ButtonChange();
-            }
-
-        }
+      
     }
-
-
-    public void ButtonChange()
-    {
-        int randnum = Random.Range(0, 2);//0,1の数を返す
-
-        Instantiate(CreateButton[randnum], ButtonGene[randnum].transform.position, Quaternion.identity);
-    }
-
 
 }
 
--- social game/Assets/Scripts/Battle/ButtonManager.cs
+++ social game/Assets/Scripts/Battle/ButtonManager.cs
@@ -1,25 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] Stage1Button;
+    [SerializeField] private GameObject Stage1Button;
 
     [SerializeField]  private GameObject battlemanager;//バトルマネージャー呼び出し
 
     BattleManager b_manager;//バトルマネージャースクリプト
+
+    [SerializeField] private GameObject[] ShaffleButton;//位置が違うボタンを用意
+
+    [SerializeField] private float changetime;//ボタン変更時間
+
+    private bool onechange;//一回の処理につき一回ボタンチェンジ
+
+    public int max_BossHP;//ボスの最大HP
 
     // Start is called before the first frame update
     void Start()
     {
         battlemanager = GameObject.Find("BattleManager");
         b_manager = battlemanager.GetComponent<BattleManager>();//スクリプト獲得
+
+        //最大HPを保存
+        max_BossHP = b_manager.B_hp;
+
+        if (b_manager.Stage_Id == 1)
+        {
+            Stage1Button.SetActive(true);
+        }
+        else if (b_manager.Stage_Id == 2)
+        {
+            Stage1Button.SetActive(true);
+            //Instantiate(CreateButton[0], Legs[randnum].transform.position, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //ボスのHPが半分切った時,ボタンチェンジ
+        if (b_manager.Stage_Id == 2 && b_manager.B_hp <= max_BossHP / 2)
+        {
+            Debug.Log("ひか");
+            changetime -= Time.deltaTime;
+
+            if (changetime <= 0 && !onechange)
+            {
+                Stage1Button.SetActive(false);
+                onechange = true;
+                ButtonChange();
+            }
+
+        }
     }
+
+    public void ButtonChange()
+    {
+        int randnum = Random.Range(0, 2);//0,1の数を返す
+
+        ShaffleButton[randnum].SetActive(true);
+    }
+
 }
